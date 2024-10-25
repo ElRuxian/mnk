0f697c6a0225433f542f525112196b127ace954b
ElRuxian <jeronimo2613@gmail.com>
2024-10-25 14:21:05 -0300

    line.hpp: use Grid and Point concepts

--- mnk/model/line.hpp
+++ mnk/model/line.hpp
@@ -9,9 +9,7 @@
 
 namespace mnk::model {
 
-template <class Point, typename ComponentType = Point::component_type,
-          size_t Dimension = Point::dimension>
-  requires std::is_same_v<Point, point<ComponentType, Dimension>>
+template <Point Point>
 class line {
  private:
   std::pair<Point, Point> endpoints_;
@@ -29,31 +27,29 @@ class line {
   }
 };
 
-template <class Grid, typename CellType = Grid::cell_type>
-  requires std::is_same_v<Grid, grid<CellType>>
-std::optional<line<typename Grid::point>> find_line(
-    const Grid& grid, const typename Grid::point& point,
+template <Grid Grid, class Point = Grid::point>
+std::optional<line<Point>> find_line(
+    const Grid& grid, const Point& point,
     std::optional<size_t> min_length_ = std::nullopt,
     std::optional<size_t> max_length_ = std::nullopt) {
   using Metric::Chebyshev;
-  using point_t = Grid::point;
 
   size_t min_possible_length = 0;
   size_t max_possible_length = norm<Chebyshev>(grid.get_size());
   size_t min_length          = min_length_.value_or(min_possible_length);
   size_t max_length          = max_length_.value_or(max_possible_length);
 
-  static constexpr auto directions = std::array<point_t, 4>{
-      point_t{-1, 1},
-      point_t{0,  1},
-      point_t{1,  0},
-      point_t{1,  1},
+  static constexpr auto directions = std::array<Point, 4>{
+      Point{-1, 1},
+      Point{0,  1},
+      Point{1,  0},
+      Point{1,  1},
   };
 
   for (auto& direction : directions) {
-    line<point_t> line        = {find_sequence_end(grid, point, direction),
-                                 find_sequence_end(grid, point, -direction)};
-    auto          line_length = length<Chebyshev>(line);
+    line<Point> line        = {find_sequence_end(grid, point, direction),
+                               find_sequence_end(grid, point, -direction)};
+    auto        line_length = length<Chebyshev>(line);
     if (line_length < min_length) continue;
     if (line_length > max_length) continue;
     return line;
