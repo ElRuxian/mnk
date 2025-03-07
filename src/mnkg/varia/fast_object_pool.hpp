a4376caca975cff921dd506fa1c14b50c968137d
ElRuxian <jeronimo2613@gmail.com>
2025-03-06 23:11:51 -0300

    add varia/fast_object_pool.hpp

---
+++ src/mnkg/varia/fast_object_pool.hpp
@@ -0,0 +1,93 @@
+// This class implements a fixed-size pool of objects that can be allocated and
+// deallocated in constant time.
+// Speed was valued over safety and functionality, as the pool was made for its
+// use in a controlled, performance-critical context: the MCTS algorithm found
+// in this project.
+
+#include <cassert>
+#include <cstddef>
+#include <new>
+#include <utility>
+#include <vector>
+
+namespace mnkg {
+
+template <typename T>
+class fast_object_pool {
+public:
+        explicit fast_object_pool(std::size_t capacity) : capacity_(capacity)
+        {
+                assert(capacity_ > 1 && "what are you doing?");
+                memory_ = ::operator new(capacity_ * sizeof(T));
+                for (std::size_t i = 0; i < capacity_; ++i)
+                        free_.push_back(reinterpret_cast<T *>(
+                            static_cast<char *>(memory_) + i * sizeof(T)));
+        }
+
+        ~fast_object_pool()
+        {
+                // WARNING: The user is responsible for deallocating all
+                // objects. Otherwise, they are deleted without proper
+                // destruction.
+                assert(free_.size() == capacity_);
+                ::operator delete(memory_);
+        }
+
+        inline std::size_t // maximum number o allocable objects
+        capacity() const
+        {
+                return capacity_;
+        }
+
+        inline std::size_t // number of allocated objects
+        size() const
+        {
+                return capacity_ - free_.size();
+        }
+
+        template <typename... Args>
+        T *
+        allocate(Args &&...args)
+        // WARNING: * Not thread-safe.
+        //          * If pool is full, returns nullptr, or aborts (debug mode).
+        {
+                assert(free_.size() > 0);
+                if (free_.empty())
+                        return nullptr;
+                T *obj = free_.back();
+                free_.pop_back();
+                new (obj) T(std::forward<Args>(args)...);
+                return obj;
+        }
+
+        void
+        deallocate(T *obj)
+        // WARNING: * Not thread-safe.
+        {
+                assert(obj >= reinterpret_cast<T *>(memory_)
+                       && obj < reinterpret_cast<T *>(memory_) + capacity_
+                       && "deallocated object not in pool");
+
+                obj->~T();
+                free_.push_back(obj);
+        }
+
+private:
+        void             *memory_;
+        const std::size_t capacity_;
+        std::vector<T *>  free_;
+
+public:
+        struct deleter {
+                fast_object_pool *pool;
+                void
+                operator()(T *ptr) const
+                {
+                        assert(ptr && "deleting nullptr");
+                        assert(pool && "no pool set");
+                        pool->deallocate(ptr);
+                }
+        };
+};
+
+} // namespace mnkg
